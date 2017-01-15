4f15fd5db14d3e7dec6dfe23877a74c032e474b9
Chris Jeong <crowjdh@gmail.com>
2017-01-15 14:37:42 +0900

    Improve example

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 import wx
-from time import sleep
 
-wx.App()
-raw_input("Press any key to exit")
+class Example(wx.Frame):
+	
+	def __init__(self, *args, **kwargs):
+		super(Example, self).__init__(*args, **kwargs) 
+			
+		self.InitUI()
+		
+	def InitUI(self):	
+		self.SetSize((300, 200))
+		self.SetTitle('Message box')
+		self.Centre()
+		self.Show(True)
+
+def main():
+	ex = wx.App()
+	Example(None)
+	ex.MainLoop()
+
+if __name__ == "__main__":
+	main()
